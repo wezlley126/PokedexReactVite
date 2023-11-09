35e273c3c7ce6219152656dfdaa39425fb846fe4
wezlley126 <weslleyborges34@gmail.com>
2023-11-09 11:56:01 -0300

    adição de botões

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ function Poke(){
         }
     )
 
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState(1)
 
     // Realiza a requisição para pokeAPI;
 
@@ -57,18 +57,34 @@ function Poke(){
         pikamon_change(value)
     }, [value])
 
+    const pika_next = () => {
+        if(value != 649){
+            setValue(value + 1)
+        }
+    }
+
+    const pika_return = () => {
+        if(value != 1){
+            setValue(value - 1)
+        }
+    }
+
     //console.log(pikamon)
 
     return(
         <>
         <div className='div_pai'>
-            <input onChange={valoro} className='searchBar' type="text" placeholder='Nome ou Id'/>
-            <span>{pikamon['id']} - {pikamon['nome']}</span>
-            <img className='pikamon_img' src={pikamon['img']} alt="" />
-            <div className='data'>
-                <span className='atk'>ATK</span><span className='def'>DEF</span><span className='life'>LIFE</span>
-                <span className='atk'>{pikamon['atk']}</span><span className='def'>{pikamon['def']}</span><span className='life'>{pikamon['hp']}</span>
+            <button onClick={pika_return} className='buttons'>Voltar</button>
+            <div className='div_main'>
+                <input onChange={valoro} className='searchBar' type="text" placeholder='Nome ou Id'/>
+                <span>{pikamon['id']} - {pikamon['nome']}</span>
+                <img className='pikamon_img' src={pikamon['img']} alt="" />
+                <div className='data'>
+                    <span className='atk'>ATK</span><span className='def'>DEF</span><span className='life'>LIFE</span>
+                    <span className='atk'>{pikamon['atk']}</span><span className='def'>{pikamon['def']}</span><span className='life'>{pikamon['hp']}</span>
+                </div>
             </div>
+            <button onClick={pika_next} className='buttons'>Proximo</button>
         </div>
         </>
     )
